13f57277aa46966521baf275d01447bc395ae67f
unknown <harrenaustint@gmail.com>
2020-06-03 09:16:55 -0400

    Updates

---
+++ precache-manifest.95cfa543028e9674ab102e7754c296dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd685507bf96a42c33d66432287762b5",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "efa69a1c6b2b3bcdd2b7",
+    "url": "/todoapp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bbdd9fd0be71d7c8ebc4",
+    "url": "/todoapp/static/js/2.7318d506.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.7318d506.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efa69a1c6b2b3bcdd2b7",
+    "url": "/todoapp/static/js/main.cd4d1631.chunk.js"
+  },
+  {
+    "revision": "7ed5984e130a809b6466",
+    "url": "/todoapp/static/js/runtime-main.10e57ee9.js"
+  }
+]);
